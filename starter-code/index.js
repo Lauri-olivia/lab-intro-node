c0b0539a6840e7629c1fdd48ddc0a8d31b3f7ca9
Laura Locher <lauralocher@Lauras-MacBook-Pro.local>
2019-06-25 11:30:00 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,34 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor(numbers) {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b); // von klein nach groß nur bei Zahlen
+    this.length++;
+  }
+  get(nth) {
+    return this.items[nth];
+  }
+  max() {
+    if (this.length == 0) throw new Error("EmptySortedList");
+    return Math.max(...this.items); // Math.max nimmt keinen array perse rein
+  }
+
+  min() {
+    if (this.length == 0) throw new Error("EmptySortedList");
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    return this.items.reduce((acc, val) => acc + val, 0);
+  }
+
+  average() {
+    if (this.length == 0) throw new Error("EmptySortedList");
+    return this.sum(this.items) / this.length;
+  }
+}
 
 module.exports = SortedList;
